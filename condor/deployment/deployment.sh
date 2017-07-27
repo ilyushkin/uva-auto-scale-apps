c3a1c21303d1e60b9268472b1ea99268e52e0936
a.s.ilyushkin@tudelft.nl <a.s.ilyushkin@tudelft.nl>
2017-07-27 16:22:26 +0200

    Updated deployment scripts.

--- condor/deployment/deployment.sh
+++ condor/deployment/deployment.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
 
+deploy_java_ubuntu() {
+add-apt-repository -y ppa:openjdk-r/ppa
+apt-get update -y
+apt-get install -y openjdk-8-jdk
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+}
+
+deploy_python_ubuntu() {
+apt-get update -y
+apt-get install -y python
+}
+
+deploy_condor_ubuntu() {
 useradd -M -N -r -s /bin/false condor
 wget http://parrot.cs.wisc.edu//symlink/20170727031502/8/8.6/8.6.4/e5147c3201f2dfa456465a19e67b313f/condor-8.6.4-x86_64_Ubuntu14-stripped.tar.gz
 tar xzf condor-8.6.4-x86_64_Ubuntu14-stripped.tar.gz
 cd condor-8.6.4-x86_64_Ubuntu14-stripped
 mkdir /scratch
 condor_install --prefix=~condor --local-dir=/scratch/condor --type=manager
+/etc/init.d/condor restart
+}
+
+#gh=https://raw.githubusercontent.com
+#curl -sSfL $gh/ilyushkin/uva-auto-scale-apps/master/hist-as/deployment/deployment.sh | bash
+
+deploy_java_ubuntu
+deploy_python_ubuntu
+deploy_condor_ubuntu
