e8f87e728dea731b4be170dbca58f09d887a0fda
a.s.ilyushkin@tudelft.nl <a.s.ilyushkin@tudelft.nl>
2017-07-27 19:40:26 +0200

    Use apt-get to install condor.

--- condor/deployment/central-manager-deployment.sh
+++ condor/deployment/central-manager-deployment.sh
@@ -15,21 +15,23 @@ apt-get install -y python
 }
 
 deploy_condor_ubuntu() {
-apt-get update -y
-apt-get install -y gcc-multilib
-useradd -M -N -r -s /bin/false condor
-condor_name=condor-8.6.4-x86_64_Ubuntu14-stripped
-wget http://parrot.cs.wisc.edu/symlink/20170727031502/8/8.6/8.6.4/e5147c3201f2dfa456465a19e67b313f/$condor_name.tar.gz
-tar xzf $condor_name.tar.gz
-rm -f $condor_name.tar.gz
-mkdir /root/condor
-mkdir /scratch
-mkdir /scratch/condor
-/root/$condor_name/condor_install --prefix=/root/condor --local-dir=/scratch/condor --type=manager,submit
-rm -rf $condor_name
-source /root/condor/condor.sh
-
-/root/condor/sbin/condor_master
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y htcondor
+#apt-get update -y
+#apt-get install -y gcc-multilib
+#useradd -M -N -r -s /bin/false condor
+#condor_name=condor-8.6.4-x86_64_Ubuntu14-stripped
+#wget http://parrot.cs.wisc.edu/symlink/20170727031502/8/8.6/8.6.4/e5147c3201f2dfa456465a19e67b313f/$condor_name.tar.gz
+#tar xzf $condor_name.tar.gz
+#rm -f $condor_name.tar.gz
+#mkdir /root/condor
+#mkdir /scratch
+#mkdir /scratch/condor
+#/root/$condor_name/condor_install --prefix=/root/condor --local-dir=/scratch/condor --type=manager,submit
+#rm -rf $condor_name
+#source /root/condor/condor.sh
+#
+#/root/condor/sbin/condor_master
 }
 
 #gh=https://raw.githubusercontent.com
