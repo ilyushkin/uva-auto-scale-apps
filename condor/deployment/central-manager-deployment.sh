0c41e943cf26eeeed292b149c347f20c07edae14
a.s.ilyushkin@tudelft.nl <a.s.ilyushkin@tudelft.nl>
2017-07-27 16:39:58 +0200

    Renamed CM deployment.

--- condor/deployment/central-manager-deployment.sh
+++ condor/deployment/central-manager-deployment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eufx -o pipefail
+
 deploy_java_ubuntu() {
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -y
@@ -15,15 +17,15 @@ apt-get install -y python
 deploy_condor_ubuntu() {
 useradd -M -N -r -s /bin/false condor
 wget http://parrot.cs.wisc.edu//symlink/20170727031502/8/8.6/8.6.4/e5147c3201f2dfa456465a19e67b313f/condor-8.6.4-x86_64_Ubuntu14-stripped.tar.gz
-tar xzf condor-8.6.4-x86_64_Ubuntu14-stripped.tar.gz
+tar xzf condor-8.6.4-x86_64_Ubuntu14-stripped.tar.gz -C /root/condor
 cd condor-8.6.4-x86_64_Ubuntu14-stripped
 mkdir /scratch
-condor_install --prefix=~condor --local-dir=/scratch/condor --type=manager
+condor_install --prefix=/root/condor --local-dir=/scratch/condor --type=manager
 /etc/init.d/condor restart
 }
 
 #gh=https://raw.githubusercontent.com
-#curl -sSfL $gh/ilyushkin/uva-auto-scale-apps/master/hist-as/deployment/deployment.sh | bash
+#curl -sSfL $gh/ilyushkin/uva-auto-scale-apps/master/hist-as/deployment/central-manager-deployment.sh | bash
 
 deploy_java_ubuntu
 deploy_python_ubuntu
