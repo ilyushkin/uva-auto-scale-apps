a617672e6d32769fca3d279828c76fcfd13459c5
Alexéy Ilyushkin <alexey.ilyushkin@gmail.com>
2017-07-31 18:27:30 +0200

    Changed the tarball link.

--- condor/deployment/central-manager-deployment.sh
+++ condor/deployment/central-manager-deployment.sh
@@ -15,30 +15,35 @@ apt-get install -y python
 }
 
 deploy_condor_ubuntu() {
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -q
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" htcondor
+#export DEBIAN_FRONTEND=noninteractive
+#apt-get update -q
+#apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" htcondor
 
-#apt-get update -y
-#apt-get install -y gcc-multilib
-#useradd -M -N -r -s /bin/false condor
-#condor_name=condor-8.6.4-x86_64_Ubuntu14-stripped
-#wget http://parrot.cs.wisc.edu/symlink/20170727031502/8/8.6/8.6.4/e5147c3201f2dfa456465a19e67b313f/$condor_name.tar.gz
-#tar xzf $condor_name.tar.gz
-#rm -f $condor_name.tar.gz
-#mkdir /root/condor
-#mkdir /scratch
-#mkdir /scratch/condor
-#/root/$condor_name/condor_install --prefix=/root/condor --local-dir=/scratch/condor --type=manager,submit
-#rm -rf $condor_name
-#cat /root/condor/condor.sh >> /etc/environment
-#
-#/root/condor/sbin/condor_master
+hostnamectl set-hostname $HOSTNAME.htcondor
+apt-get update -y
+apt-get install -y gcc-multilib
+useradd -M -N -r -s /bin/false condor
+condor_name=condor-8.6.4-x86_64_Ubuntu14-stripped
+wget -O $condor_name.tar.gz https://github.com/ilyushkin/uva-auto-scale-apps/blob/master/condor/app/$condor_name.tar.gz?raw=true
+tar xzf $condor_name.tar.gz
+rm -f $condor_name.tar.gz
+mkdir /root/condor
+mkdir /scratch
+mkdir /scratch/condor
+/root/$condor_name/condor_install --prefix=/root/condor --local-dir=/scratch/condor --type=manager,submit
+rm -rf /root/$condor_name
+cat /root/condor/condor.sh >> /root/.bashrc
+
+/root/condor/sbin/condor_master
+/root/condor/sbin/condor_collector
+/root/condor/sbin/condor_negotiator
+/root/condor/sbin/condor_schedd
 }
 
 #gh=https://raw.githubusercontent.com
 #curl -sSfL $gh/ilyushkin/uva-auto-scale-apps/master/hist-as/deployment/central-manager-deployment.sh | bash
 
+#apt-get install software-properties-common
 deploy_java_ubuntu
 deploy_python_ubuntu
 deploy_condor_ubuntu
