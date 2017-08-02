a4c2190b9b07b3ff6d87b5c9a3dd01f463431ee4
Alexéy Ilyushkin <alexey.ilyushkin@gmail.com>
2017-08-02 13:44:09 +0200

    Added hostname in /etc/hosts

--- condor/deployment/central-manager-deployment.sh
+++ condor/deployment/central-manager-deployment.sh
@@ -2,6 +2,22 @@
 
 set -eufx -o pipefail
 
+_configure_selinux() {
+[ -f /selinux/enforce ] && echo 0 > /selinux/enforce || true
+[ -f /etc/sysconfig/selinux ] && sed -i -e 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux || true
+[ -f /etc/selinux/config ] && sed -i -e 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config || true
+}
+
+_disable_ipv6() {
+cat > /etc/sysctl.conf <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+sudo sysctl -p
+cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+}
+
 deploy_java_ubuntu() {
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -y
@@ -16,9 +32,16 @@ apt-get install -y python
 
 deploy_condor_ubuntu() {
 hostnamectl set-hostname $HOSTNAME.local
+var="127.0.0.1 localhost localhost.localdomain $HOSTNAME"
+sed -i "1s/.*/$var/" /etc/hosts
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -q
 apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" htcondor
+
+# modify the config here /etc/condor/condor_config
+# DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD, STARTD
+# DEFAULT_DOMAIN_NAME = $(HOSTNAME)
+# COLLECTOR_HOST = $(HOSTNAME)
 }
 
 deploy_pegasus() {
@@ -26,6 +49,9 @@ wget -O - http://download.pegasus.isi.edu/pegasus/gpg.txt | sudo apt-key add -
 echo 'deb [arch=amd64] http://download.pegasus.isi.edu/pegasus/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/pegasus.list
 apt-get update -q
 apt-get install pegasus -y
+export PYTHONPATH=`pegasus-config --python`
+export PERL5LIB=`pegasus-config --perl`
+export CLASSPATH=`pegasus-config --classpath`
 }
 
 #apt-get install software-properties-common
