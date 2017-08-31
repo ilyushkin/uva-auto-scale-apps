06cf744c24164596468137fdf074f596317497c9
Alexéy Ilyushkin <alexey.ilyushkin@gmail.com>
2017-08-31 20:02:32 +0200

    Added ALLOW_WRITE.

--- condor/deployment/central-manager-deployment.sh
+++ condor/deployment/central-manager-deployment.sh
@@ -40,6 +40,7 @@ apt-get update -q
 apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" htcondor
 echo "CONDOR_HOST = \$(HOSTNAME)" >> /etc/condor/condor_config.local
 echo "COLLECTOR_HOST = \$(CONDOR_HOST)" >> /etc/condor/condor_config.local
+echo "ALLOW_WRITE = *" >> /etc/condor/condor_config.local
 echo "DAEMON_LIST = MASTER, COLLECTOR, NEGOTIATOR, SCHEDD" >> /etc/condor/condor_config.local
 ss-set cm.hostname $cm_hostname
 eth0_ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
